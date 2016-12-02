b41cbae4fa6f7e28a3155bfc6efb4d4b31b2c707
Raphael Tan <raphaeltanyw@gmail.com>
2016-12-02 14:22:44 +0800

    Refactor Tweets and DevQuotes

--- lib/app/tweets.ex
+++ lib/app/tweets.ex
@@ -1,17 +1,13 @@
 defmodule App.Tweets do
-  use GenServer
-  @name __MODULE__
-
   def start_link do
-    Agent.start_link(fn -> [DateTime.utc_now] end, name: @name)
-    add_tweets(fetch_from_twitter)
+    Agent.start_link(fn -> fetch_from_twitter end, name: __MODULE__)
   end
 
   def get_random do
-    Agent.get(@name, &(Enum.random(tl(&1))))
+    Agent.get(__MODULE__, &(Enum.random(tl(&1))))
   end
 
-  def fetch_from_twitter() do
+  defp fetch_from_twitter() do
     opts = [screen_name: "50nerdsofgrey", count: 200]
     current_batch = ExTwitter.user_timeline(opts)
 
@@ -31,21 +27,5 @@ defmodule App.Tweets do
     else
       (current_batch |> Enum.map(&(&1.text))) ++ fetch_from_twitter(List.last(current_batch).id)
     end
-  end
-
-  defp add_tweets(tweets) do
-    Agent.update(@name, &do_add_tweets(&1, tweets))
-  end
-
-  defp get_size do
-    Agent.get(@name, &(length(&1) - 1))
-  end
-
-  defp do_add_tweets(list, tweets) do
-    list ++ tweets
-  end
-
-  defp get_last_update_time do
-    Agent.get(@name, &(hd(&1)))
   end
 end
